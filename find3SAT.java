2f785e68659c50e8e3a9dd941cba0ff783b0bff1
Aubrey Jumonville <ajumonville@precisionplanting.com>
2023-03-07 14:38:07 -0600

    update a

--- find3SAT.java
+++ find3SAT.java
@@ -2,59 +2,78 @@ package groupAssignment;
 
 import java.util.*;
 
-public class find3SAT{
+public class find3SAT {
 
     public static void main(String[] args) {
         String fileName = "";
         if (args.length > 0) {
             fileName = args[0];
         }
-        //Initialize reducer
+        // Initialize reducer
         Reducer r = new Reducer();
 
-        //initialize reader that breaks down given file into the agreed upon type
+        // initialize reader that breaks down given file into the agreed upon type
         FileReader reader = new FileReader(fileName);
         ArrayList<ArrayList<Integer>> cnfs = reader.cnf();
 
-        //print initialization
+        // print initialization
         System.out.println("* Solve 3CNF in 3cnfs.txt:(reduced to K-Clique) *");
         System.out.println("X means either T or F");
 
-        //loop through each 3cnf
+        // loop through each 3cnf
         for (int i = 0; i < cnfs.size(); i++) {
-            //start timer
+            boolean answer = true;
+            // start timer
             long startTime = System.currentTimeMillis();
             ArrayList<Integer> current = cnfs.get(0);
-            //reduce to vertex cover
+            // reduce to vertex cover
             int variables = Collections.max(current);
             int[][] vertexCover = r.toVertexCover(current, variables * 2);
             ArrayList<Integer> maximumClique = r.toMaxClique(r.toCliqueProblem(vertexCover));
-        
-            int[] assigments = new int[variables];
 
-            for (int j = 0; j < maximumClique.size(); j++) {
-                int temp = maximumClique.get(j);
-                if (temp < variables * 2) {
-                    assigments[temp/2] = (temp%2)+ 1;
-                }else{
-                    break;
+            // determine if there is an answer based on size of clique in relation to k
+            if (maximumClique.size() >= (variables + (current.size() / 3 * 2))) {
+                answer = false;
+            }
+
+            int[] assignments = new int[variables];
+
+            if (answer) {
+                for (int j = 0; j < maximumClique.size(); j++) {
+                    int temp = maximumClique.get(j);
+                    if (temp < variables * 2) {
+                        assignments[temp / 2] = (temp % 2) + 1;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                Random rd = new Random();
+                for (int j = 0; j < variables; j++) {
+                    assignments[j] = ((rd.nextInt() % 2) + 1);
                 }
             }
-            //stop timer
+
+            // stop timer
             long ms = System.currentTimeMillis() - startTime;
 
-            //print answer
-            System.out.print("3CNF No." + (i + 1) + ":[n=" + Collections.max(current) +  " k=" + current.size() / 3);
-            System.out.print("] (" + ms + "ms) Solution:[");
-               
-            for (int variable = 0; variable < assigments.length; variable++) {
+            // print answer
+            System.out.print("3CNF No." + (i + 1) + ":[n=" + Collections.max(current) + " k=" + current.size() / 3);
+            System.out.print("] (" + ms + "ms) ");
+            if (answer) {
+                System.out.print("Solution:[");
+            } else {
+                System.out.print("No solution! Random:[");
+            }
+
+            for (int variable = 0; variable < assignments.length; variable++) {
                 System.out.print((variable + 1) + ":");
-                if (assigments[variable] == 0) {
+                if (assignments[variable] == 0) {
                     System.out.print("X ");
-                } else if (assigments[variable] == 1) {
+                } else if (assignments[variable] == 1) {
                     System.out.print("T ");
                 } else {
-                   System.out.print("F " );
+                    System.out.print("F ");
                 }
             }
             System.out.print("]\n");
@@ -83,22 +102,24 @@ public class find3SAT{
             System.out.print(" ==>\n");
 
             System.out.print("(");
-             if (assigments[0] == 0) {
-                    System.out.print(" X");
-                } else if (assigments[Math.abs(current.get(0))-1] == 1 && current.get(0) > 0 || assigments[Math.abs(current.get(0))-1] == 1 && current.get(0) < 0) {
-                    System.out.print(" T");
-                } else {
-                    System.out.print(" F");
-                }
+            if (assignments[0] == 0) {
+                System.out.print(" X");
+            } else if (assignments[Math.abs(current.get(0)) - 1] == 1 && current.get(0) > 0
+                    || assignments[Math.abs(current.get(0)) - 1] == 1 && current.get(0) < 0) {
+                System.out.print(" T");
+            } else {
+                System.out.print(" F");
+            }
             System.out.print("|");
             for (int z = 1; z < current.size(); z++) {
-                int value = Math.abs(current.get(z))-1;
+                int value = Math.abs(current.get(z)) - 1;
                 if (z % 3 == 0) {
                     System.out.print("∧(");
                 }
-                if (assigments[value] == 0) {
+                if (assignments[value] == 0) {
                     System.out.print(" X");
-                } else if (assigments[value] == 1 && current.get(z) > 0 || assigments[value] == 1 && current.get(z) < 0) {
+                } else if (assignments[value] == 1 && current.get(z) > 0
+                        || assignments[value] == 1 && current.get(z) < 0) {
                     System.out.print(" T");
                 } else {
                     System.out.print(" F");
@@ -110,10 +131,10 @@ public class find3SAT{
                 }
 
             }
-            
+
             System.out.print("\n\n");
 
-        }  
+        }
         return;
     }
 }
